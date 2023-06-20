c641be06dd5faa64810cd4d6d9de4e722c9b88c5
Antoan Furlanski <antoan.furlanski@gmail.com>
2023-06-20 18:16:28 +0300

    abstraction

---
+++ Abstraction/Cars/ICar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public interface ICar
+    {
+        
+        public string Model { get; }
+        public string Color { get; }
+
+        string Start();
+        string Stop();
+
+    }
+}
