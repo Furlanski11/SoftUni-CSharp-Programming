6c018e712a0ef4e662df2afbfe676198e04fcfb6
Antoan Furlanski <antoan.furlanski@gmail.com>
2023-06-26 21:23:24 +0300

    Encapsulation

---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+5
+Andrew Williams 20 2200
+Newton Holland 57 3333
+Rachelle Nelson 27 670
+Grigor Dimitrov 25 666.66
+Brandi Scott 35 50404
---
+++ Encapsulation/PersonsInfo/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+
+            var persons = new List<Person>();
+            Team team = new("SoftUni");
+            for (int i = 0; i < lines; i++)
+            {
+                string[] line = Console.ReadLine().Split(" ");
+                Person person = new(line[0], line[1], int.Parse(line[2]), decimal.Parse(line[3]));
+                team.AddPlayer(person);
+            }
+            Console.WriteLine(team);
+            
+        }
+
+    }
+}
