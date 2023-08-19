6f0e0fb7588c532e9f4e56ba7db7940d60b814dd
Antoan Furlanski <antoan.furlanski@gmail.com>
2023-08-19 20:47:27 +0300

    .

---
+++ CSharpAdvanced/GenericsExc/BoxOfInts/Box.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfInts
+{
+    public class Box<T>
+    {
+        private List<T> Ints;
+
+        public Box()
+        {
+            Ints = new List<T>();
+        }
+        public void Add(T item)
+        {
+            Ints.Add(item);
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in Ints)
+            {
+                sb.AppendLine($"{item.GetType()}: {item}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ CSharpAdvanced/GenericsExc/BoxOfInts/Program.cs
@@ -0,0 +1,12 @@
+﻿using BoxOfInts;
+using System;
+
+int n = int.Parse(Console.ReadLine());
+
+Box<int> box = new();
+for (int i = 0; i < n; i++)
+{
+    box.Add(int.Parse(Console.ReadLine()));
+}
+
+Console.WriteLine(box.ToString());
