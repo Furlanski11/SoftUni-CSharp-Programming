ec92b6d158476b1bb5cd4503125a42144bfb7a03
Antoan Furlanski <antoan.furlanski@gmail.com>
2023-08-21 21:39:38 +0300

    s

---
+++ CSharpAdvanced/GenericsExc/CountMethodDoubles/Program.cs
@@ -0,0 +1,14 @@
+﻿using CountMethodDoubles;
+using System;
+
+int n = int.Parse(Console.ReadLine());
+Box<double> box = new Box<double>();
+
+for (int i = 0; i < n; i++)
+{
+    box.Add(double.Parse(Console.ReadLine()));
+}
+
+double comparer = double.Parse(Console.ReadLine());
+
+Console.WriteLine(box.Bigger(comparer));
