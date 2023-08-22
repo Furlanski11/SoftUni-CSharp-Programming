7adc397c86c903d7c72fbcb1f832f0362be85e7d
Antoan Furlanski <antoan.furlanski@gmail.com>
2023-08-22 23:37:59 +0300

    Count Method Strings

---
+++ CSharpAdvanced/GenericsExc/CountMethodStrings/Box.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountMethodStrings
+{
+    public class Box<T> where T : IComparable<T>
+    {
+        private List<T> Strings;
+
+        public Box()
+        {
+            Strings = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            Strings.Add(item);
+        }
+
+        public List<T> Swap(int first, int second)
+        {
+            T firstItem = Strings[first];
+            T secondItem = Strings[second];
+
+            Strings[first] = secondItem;
+            Strings[second] = firstItem;
+
+            return Strings;
+        }
+
+        public int Bigger(T comparer)
+        {
+            int count = 0;
+            foreach (var item in Strings)
+            {
+                if (item.CompareTo(comparer) == 1)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in Strings)
+            {
+                sb.AppendLine($"{item.GetType()}: {item}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+
+}
---
+++ CSharpAdvanced/GenericsExc/CountMethodStrings/Program.cs
@@ -0,0 +1,16 @@
+﻿using CountMethodStrings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+int n = int.Parse(Console.ReadLine());
+Box<string> box = new Box<string>();
+
+for (int i = 0; i < n; i++)
+{
+    box.Add(Console.ReadLine());
+}
+
+string comparer = Console.ReadLine();
+
+Console.WriteLine(box.Bigger(comparer));
