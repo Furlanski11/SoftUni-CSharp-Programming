eecc5a14aeeeb3166d259513d2310ddf766c4a7d
Antoan Furlanski <antoan.furlanski@gmail.com>
2023-08-24 23:47:36 +0300

    Swap Ints

---
+++ CSharpAdvanced/GenericsExc/SwapInts/Program.cs
@@ -0,0 +1,21 @@
+﻿using SwapInts;
+using System;
+
+using System.Collections.Generic;
+using System.Linq;
+
+int n = int.Parse(Console.ReadLine());
+Box<int> box = new Box<int>();
+
+for (int i = 0; i < n; i++)
+{
+    box.Add(int.Parse(Console.ReadLine()));
+}
+
+int[] array = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+int first = array[0];
+int second = array[1];
+
+box.Swap(first, second);
+
+Console.WriteLine(box.ToString());
