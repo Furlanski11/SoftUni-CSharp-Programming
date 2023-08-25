cf47a8dd96a92c430929ff872c87c0124f6b3ba0
Antoan Furlanski <antoan.furlanski@gmail.com>
2023-08-25 22:00:59 +0300

    swap strings

---
+++ CSharpAdvanced/GenericsExc/SwapStrings/Box.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapStrings
+{
+    public class Box<T>
+    {
+        private List<T> Strings;
+
+        public Box()
+        {
+            Strings = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            Strings.Add(item);
+        }
+
+        public List<T> Swap(int first, int second)
+        {
+            T firstItem = Strings[first];
+            T secondItem = Strings[second];
+
+            Strings[first] = secondItem;
+            Strings[second] = firstItem;
+
+            return Strings;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in Strings)
+            {
+                sb.AppendLine($"{item.GetType()}: {item}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ CSharpAdvanced/GenericsExc/SwapStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿using SwapStrings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+int n = int.Parse(Console.ReadLine());
+Box<string> box = new Box<string>();
+
+ for(int i = 0;i < n; i++)
+ {
+     box.Add(Console.ReadLine());
+ }
+ 
+ int[] array = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+ int first = array[0];
+ int second = array[1];
+ 
+ box.Swap(first, second);
+ 
+ Console.WriteLine(box.ToString()); 
