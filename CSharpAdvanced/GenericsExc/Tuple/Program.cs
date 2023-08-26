45545be607bfb2673868b62392a0ca1d56f8a9f3
Antoan Furlanski <antoan.furlanski@gmail.com>
2023-08-26 20:49:01 +0300

    Tuple

---
+++ CSharpAdvanced/GenericsExc/Tuple/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Tuple;
+
+string nameAdress = Console.ReadLine();
+
+string[] names = nameAdress.Split(" ");
+
+CustomTuple<string, string> tupleNames = new($"{names[0]} {names[1]}", names[2]);
+
+string beers = Console.ReadLine();
+
+string[] beersInput = beers.Split(" ");
+
+CustomTuple<string, int> tupleBeers = new(beersInput[0], int.Parse(beersInput[1]));
+
+string[] numbers = Console.ReadLine().Split(" ");
+
+CustomTuple<int, double> nums = new(int.Parse(numbers[0]), double.Parse(numbers[1]));
+
+Console.WriteLine(tupleNames);
+Console.WriteLine(tupleBeers);
+Console.WriteLine(nums);
