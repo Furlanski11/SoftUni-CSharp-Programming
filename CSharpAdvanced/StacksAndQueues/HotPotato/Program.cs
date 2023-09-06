deae6f36eb2f8db4bcc6734a257ee5b8d0a52d9b
Antoan Furlanski <antoan.furlanski@gmail.com>
2023-09-06 13:04:44 +0300

    Hot Potato

---
+++ CSharpAdvanced/StacksAndQueues/HotPotato/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StacksAndQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> children = Console.ReadLine().Split(" ").ToList();
+            Queue<string> ingameKids = new Queue<string>(children);
+            int throws = int.Parse(Console.ReadLine());
+            string currentKid = string.Empty;
+            while (ingameKids.Count != 1)
+            {
+                for (int i = 1; i < throws; i++)
+                {
+                    currentKid = ingameKids.Dequeue();
+                    ingameKids.Enqueue(currentKid);
+                }
+                Console.WriteLine($"Removed {ingameKids.Dequeue()}");
+            }
+            Console.WriteLine($"Last is {ingameKids.Dequeue()}");
+        }
+    }
+}
