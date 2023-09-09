e07d2c6b7126f7198bbfa6c57043454c81553927
Antoan Furlanski <antoan.furlanski@gmail.com>
2023-09-09 20:40:27 +0300

    Simple Calculator

---
+++ CSharpAdvanced/StacksAndQueues/SimpleCalculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StacksAndQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] splitedInput = input.Split(" ");
+            Stack<string> stack = new Stack<string>(splitedInput.Reverse());
+            int sum = int.Parse(stack.Pop());
+            while (stack.Count != 0)
+            {
+                string currentPop = stack.Pop();
+                if (currentPop == "+")
+                {
+                    sum += int.Parse(stack.Pop());
+                }
+                else if (currentPop == "-")
+                {
+                    sum -= int.Parse(stack.Pop());
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
