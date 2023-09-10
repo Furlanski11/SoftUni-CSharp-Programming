877d389ed6052fedd9384c7ae9231df5b8ec24a4
Antoan Furlanski <antoan.furlanski@gmail.com>
2023-09-10 21:28:15 +0300

    Matching Brackets

---
+++ CSharpAdvanced/StacksAndQueues/MatchingBrackets/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StacksAndQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<int> firstbrackets = new Stack<int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    firstbrackets.Push(i);
+                }
+                if (input[i] == ')')
+                {
+                    int firstBracket = firstbrackets.Pop();
+                    for (int j = firstBracket; j <= i; j++)
+                    {
+                        Console.Write(input[j]);
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
