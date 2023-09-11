39c2393f166e6610d696384a1e6ae60ac60e3024
Antoan Furlanski <antoan.furlanski@gmail.com>
2023-09-11 23:14:48 +0300

    Print even numbers

---
+++ CSharpAdvanced/StacksAndQueues/PrintEventNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StacksAndQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split(" ").Select(int.Parse).ToArray();
+            Queue<int> numbers = new Queue<int>(nums);
+            List<int> evenNumbers = new List<int>();
+            while (numbers.Count != 0)
+            {
+                int curNum = numbers.Dequeue();
+                if (curNum % 2 == 0)
+                {
+                    evenNumbers.Add(curNum);
+                }
+            }
+            Console.WriteLine(string.Join(", ", evenNumbers));
+        }
+    }
+}
