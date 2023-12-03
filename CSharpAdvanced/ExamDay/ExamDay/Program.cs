a31c260227ec5b989c013c1fb4369bba2bb257ce
Antoan Furlanski <antoan.furlanski@gmail.com>
2023-12-03 14:20:10 +0200

    Final Exam

---
+++ CSharpAdvanced/ExamDay/ExamDay/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+Queue<int> textile = new(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+Stack<int> meds = new(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+Dictionary<string, int> doneMeds = new();
+
+doneMeds.Add("Patch", 0);
+doneMeds.Add("Bandage", 0);
+doneMeds.Add("MedKit", 0);
+
+while (true)
+{
+     if (textile.Count == 0 && meds.Count == 0)
+    {
+        Console.WriteLine("Textiles and medicaments are both empty.");
+        foreach (var item in doneMeds.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+        {
+            if (item.Value > 0)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value}");
+            }
+
+        };
+        break;
+    }
+    else if (textile.Count == 0)
+    {
+        Console.WriteLine("Textiles are empty.");
+
+        foreach (var item in doneMeds.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+        {
+            if (item.Value > 0)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value}");
+            }
+         
+        }
+        if (meds.Any())
+        {
+            Console.Write("Medicaments left: ");
+            Console.WriteLine(string.Join(", ", meds));
+        }
+        break;
+    }
+    else if (meds.Count == 0)
+    {
+        Console.WriteLine("Medicaments are empty.");
+
+        foreach (var item in doneMeds.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+        {
+            if (item.Value > 0)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value}");
+            }
+        }
+        if (textile.Any())
+        {
+            Console.Write("Textiles left: ");
+            Console.WriteLine(string.Join(", ", textile));
+        }
+        break;
+    }
+    int currentTextile = textile.Peek();
+    int currentMed = meds.Peek();
+    int sum = currentTextile + currentMed;
+    int leftover = 0;
+    if (sum == 30)
+    {
+        doneMeds["Patch"]++;
+        textile.Dequeue();
+        meds.Pop();
+    }
+    else if (sum == 40)
+    {
+        doneMeds["Bandage"]++;
+        textile.Dequeue();
+        meds.Pop();
+    }
+    else if (sum == 100)
+    {
+        doneMeds["MedKit"]++;
+        textile.Dequeue();
+        meds.Pop();
+    }
+    else if(sum >= 100)
+    {
+        doneMeds["MedKit"]++;
+        leftover = sum -= 100;
+        textile.Dequeue();
+        if(meds.Count > 1)
+        {
+            meds.Pop();
+            meds.Push(meds.Pop() + leftover);
+        }
+        else
+        {
+            meds.Pop();
+        }
+        
+    }
+    else
+    {
+        meds.Pop();
+        meds.Push(currentMed + 10);
+        textile.Dequeue();
+    }
+
+}
+
---
+++ CSharpAdvanced/ExamDay/SecondProblem/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+string[] size = Console.ReadLine().Split();
+int rows = int.Parse(size[0]);
+int cols = int.Parse(size[1]);
+char[,] matrix = new char[rows, cols];
+int myRow = -1, myCol = -1;
+for (int row = 0; row < rows; row++)
+{
+    string[] input = Console.ReadLine().Split();
+    for (int col = 0; col < cols; col++)
+    {
+        char currentPlace = input[col][0];
+        matrix[row, col] = currentPlace;
+        if (currentPlace == 'B')
+        {
+            myRow = row;
+            myCol = col;
+        }
+    }
+}
+
+
+int touchedOpponents = 0;
+int moves = 0;
+while (true)
+{
+    string command = Console.ReadLine();
+    if (command == "Finish")
+    {
+        break;
+    }
+    int currentRow = 0, currentCol = 0;
+    switch (command)
+    {
+        case "up": currentRow = -1; break;
+        case "down": currentRow = 1; break;
+        case "left": currentCol = -1; break;
+        case "right": currentCol = 1; break;
+    }
+    int newRow = myRow + currentRow;
+    int newCol = myCol + currentCol;
+    if (newRow < 0)
+    {
+        newRow = 0;
+    }
+
+    if (newRow >= rows)
+    {
+        newRow = rows - 1;
+    }
+
+    if (newCol < 0)
+    {
+        newCol = 0;
+    }
+
+    if (newCol >= cols)
+    {
+        newCol = cols - 1;
+    }
+
+    char newCell = matrix[newRow, newCol];
+    if (newCell == 'O')
+    {
+        continue;
+    }
+    else if (newCell == 'P')
+    {
+        touchedOpponents++;
+        moves++;
+        matrix[myRow, myCol] = '-';
+        myRow = newRow;
+        myCol = newCol;
+        matrix[myRow, myCol] = 'B';
+        if (touchedOpponents == 3)
+        {
+            break;
+        }
+    }
+    else if (newCell == '-')
+    {
+        moves++;
+        matrix[myRow, myCol] = '-';
+        myRow = newRow;
+        myCol = newCol;
+        matrix[myRow, myCol] = 'B';
+    }
+}
+
+
+Console.WriteLine("Game over!");
+Console.WriteLine($"Touched opponents: {touchedOpponents} Moves made: {moves}");
